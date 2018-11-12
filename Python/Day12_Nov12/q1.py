b3357ccaed1da6b9671580dbf4cf447711a7a0e7
aravind <you@example.com>
2018-11-12 18:44:43 +0530

    3

--- Python/Day12_Nov12/q1.py
+++ Python/Day12_Nov12/q1.py
@@ -4,9 +4,8 @@ rm=dtree.documentElement
 for bm in rm.childNodes:
 	if bm.nodeType==1:
 		print "-----------------------"
-		for k in bm.attributes.key():
-			print bm.attributes[k].names," : ",bm.attributes[k].value
+		for k in bm.attributes.keys():
+			print bm.attributes[k].name.title()," : ",bm.attributes[k].value.title()
 	for tn in bm.childNodes:
 		for tx in tn.childNodes:
 			print tx.parentNode.nodeName.title()," : ",tx.nodeValue
-	print "-----------------------"
