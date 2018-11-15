cc5ba0d3ea13a58bf465fb6c54a0a445a9caf44f
aravind <you@example.com>
2018-11-15 22:42:12 +0530

    Updated

--- ML/Day5_Nov15/q4.py
+++ ML/Day5_Nov15/q4.py
@@ -1,4 +1,4 @@
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from  sklearn import metrics
 import matplotlib.pyplot as plt
@@ -18,7 +18,7 @@ data=data.as_matrix()
 X=data[:,[0,2,4,5,6,7,8,9,10,11]]
 y=data[:,1]
 X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2)
-lr=LinearRegression()
+lr=LogisticRegression()
 lr.fit(X_train,y_train)
 p=lr.predict(X_test)
 print "Mean Absolute Error",metrics.mean_absolute_error(y_test,p)
