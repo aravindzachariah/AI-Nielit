5ebf777878df5558e83ac04dc978485ad749259e
aravind <you@example.com>
2018-11-16 21:14:27 +0530

    Updated

--- Python/Day15_Nov16/q1.py
+++ Python/Day15_Nov16/q1.py
@@ -1,13 +1,13 @@
 
 import re
-def isInteger(s):
-	p=re.compile('(?=[0-9])')
+def IsInteger(s):
+	p=re.compile('?=[\d]')
 	r=re.match(p,s)
 	if r:
 		return True
 	return False
-def isFloat(s):
-	p=re.compile('(?=\d)')
+def IsFloat(s):
+	p=re.compile('?=[\d+.\d+]')
 	r=re.match(p,s)
 	if r:
 		return True
@@ -18,14 +18,35 @@ def HasVowel(s):
 	if r:
 		return True
 	return False
-def isHex(s):
-	p=re.compile('.*(?=[\u]).*')
+def IsHex(s):
+	p=re.compile('.*(?=[0-9,a-f]).*')
 	r=re.match(p,s)
 	if r:
 		return True
 	return False
+def IsDate(s):
+	p=re.compile('.*(?=[\d\d-\d\d-\d\d\d\d]).*')
+        r=re.match(p,s)
+        if r:
+                return True
+        return False
+def IsValidPassword(s):
+        p=re.compile('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,16})')
+        r=re.match(p,s)
+        if r:
+                return True
+        return False
+def IsValidEmail(s):
+        p=re.compile('^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$')
+        r=re.match(p,s)
+        if r:
+                return True
+        return False
+
 st=raw_input("Enter the string: ")
-print "Contains Intiger : ",isInteger(st)
-print "Contains Float : ",isFloat(st)
+print "Contains Intiger : ",IsInteger(st)
+print "Contains Float : ",IsFloat(st)
 print "Contains Vowels : ",HasVowel(st)
-print "Contains Hex : ",isHex(st)
+print "Contains Hex : ",IsHex(st)
+print "Strong Password : ",IsValidPassword(st)
+print "Valid Email : ",IsValidEmail(st)
