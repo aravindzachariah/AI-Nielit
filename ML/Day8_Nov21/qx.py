dab5c67e8adcc456a34ccf2134d24cce0d023acd
aravind <you@example.com>
2018-11-21 22:06:12 +0530

    Update

--- ML/Day8_Nov21/qx.py
+++ ML/Day8_Nov21/qx.py
@@ -7,36 +7,45 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.naive_bayes import GaussianNB
 from matplotlib import pyplot as plt
 import pandas as pd
-from tkinter import filedialog
-from tkinter import * 
+from sklearn import preprocessing
+#from Tkinter import filedialog
+import tkFileDialog as filedialog
+from Tkinter import * 
+import Tkinter as tk
 wm = Tk()
-T = Text(wm, height=15, width=200)
+L = tk.Listbox(wm,width=50,height=10)
 t1=StringVar()
 t2=StringVar()
 global fl,data,il,ol
 S=""
 def fS():
 	global fl,data
-	wm.filename =  filedialog.askopenfilename(initialdir = "~",title = "Select file",filetypes = (("CSV files","*.csv"),("all files","*.*")))
+	wm.filename=filedialog.askopenfilename(initialdir = "~",title = "Select file",filetypes = (("CSV files","*.csv"),("all files","*.*")))
 	fl=wm.filename
-	data=pd.read_csv(fl,delimiter="\s+")
+	data=pd.read_csv(fl)
 	k=list(data)
-	j=0
 	for i in k:
-		S=str(i)+" "+str(j)+"\t\t"
-		j+=1
-		T.insert(END,S)
+		S=str(i)
+		L.insert(END,S)
 def rI():
 	global il
 	il=t1.get()
-	il=int(il)
-	print type(il)
 def rO():
 	global ol
 	ol=t2.get()
 def Comp():
 	global il,ol,data
+	le=preprocessing.LabelEncoder()
+	for col in data.columns.values:
+		if data[col].dtypes=='object':
+			le.fit(data[col].values)
+       			data[col]=le.transform(data[col])
+	data=data.dropna(axis=0, how='any')
 	data=data.as_matrix()
+	il=il.split(",")
+	il=[int(x) for x in il]
+	ol=ol.split(",")
+        ol=[int(x) for x in ol]
 	X=data[:,il]
 	y=data[:,ol]
 	kfold=KFold(10,random_state=7)
@@ -45,13 +54,13 @@ def Comp():
 	models.append(("NB",GaussianNB()))
 	models.append(("LG",LogisticRegression()))
 	models.append(("CART",DecisionTreeClassifier()))
-	models.append(("SVM",SVC()))
+	#models.append(("SVM",SVC()))
 	results=[]
 	names=[]
 	scoring='accuracy'
 	for name,model in models:
 		kfold=KFold(n_splits=10,random_state=7) 
-		v=cross_val_score(model,X,y,cv=kfold,scoring=scoring)
+		v=cross_val_score(model,X,y,cv=kfold,scoring="accuracy")
 		results.append(v)
 		names.append(name)
 		print(name)
@@ -62,11 +71,11 @@ def Comp():
 	plt.boxplot(results)
 	ax.set_xticklabels(names)
 	plt.show()
-wm.title("Algorith Selector ")
+wm.title("Algorithm Selector ")
 wm.geometry('800x600')
 b1=Button(wm,text='Select Data File',command=fS)
 b1.pack()
-T.pack()
+L.pack()
 e1=Entry(wm,textvariable=t1)
 e1.pack()
 b2=Button(wm,text='Set Input',command=rI)
