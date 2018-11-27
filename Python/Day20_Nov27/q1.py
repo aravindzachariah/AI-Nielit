71ba5e5d7dc1492278d1ae96d190e56c7b99bb4d
AI13 <aravindanil000@gmail.com>
2018-11-27 17:12:30 +0530

    Updated

--- Python/Day20_Nov27/q1.py
+++ Python/Day20_Nov27/q1.py
@@ -1,5 +1,13 @@
 import numpy as np
-import pandas as pd
-dt1=np.dtype([('ename','S20'),('eno','i8'),('edesign','i8'),('esalary','f8'),('ephno','S15')])
+dt1=np.dtype([('ename','S20'),('eno','i8'),('edesign','S10'),('esalary','f8'),('ephno','S15')])
 alf=np.loadtxt('emp.csv',delimiter=",",dtype=dt1)
-print alf
+print "All the records : \n ",alf
+print "\n Names of officers : \n ",alf[alf['edesign']=='officer']['ename']
+print "\n Scientist Records : \n ",alf[alf['edesign']=='scientist']
+print "\n Total Salary : \n",alf['esalary'].sum()
+print "\n Average Salary of Engineers : \n",alf[alf['edesign']=='engineer']['esalary'].mean()
+print "\n Emplyees in Acending order of salary : \n ",np.sort(alf,order='esalary')
+print "\n Emplyees in Acending order designation of salary : \n ",np.sort(np.sort(alf,order='edesign'),order='esalary')
+print "\n Lowest salary for Scientist Designation : \n ",np.sort(alf[alf['edesign']=='scientist'],order='esalary')[0]
+alf1=alf[alf['edesign']=='scientist']
+print "\n Employees who are scientists with lowest salary : \n",alf1[alf1['esalary']==alf1['esalary'].min()]
