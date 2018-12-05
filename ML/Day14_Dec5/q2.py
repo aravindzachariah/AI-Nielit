6a0b050fc2e50b6ef4dcfd0134bd725a372c6441
aravind <you@example.com>
2018-12-05 20:48:54 +0530

    Update

--- ML/Day14_Dec5/q2.py
+++ ML/Day14_Dec5/q2.py
@@ -1,12 +1,22 @@
+#Import required modules
 from sklearn.datasets import load_boston
 from sklearn.neural_network import MLPRegressor as MLP
 from sklearn.metrics import mean_absolute_error,mean_squared_error 
+import matplotlib.pyplot as plt
+
+#Load Dataset
 data=load_boston()
 X=data.data
 y=data.target
-mlp=MLP(solver='lbfgs',hidden_layer_sizes=(800),activation='tanh',max_iter=800,verbose=True)
+
+#Configure Multilayer Perceptron,Train,Predict
+mlp=MLP(solver='lbfgs',hidden_layer_sizes=(800),activation='tanh',max_iter=2000,verbose=True)
 mlp.fit(X,y)
 p=mlp.predict(X)
+
+#Result
 print "\n Mean Squared Error : ",mean_squared_error(p,y)
 print "\n Mean Absolute Error : ",mean_absolute_error(p,y)
+plt.scatter(y,p)
+plt.show()
 
